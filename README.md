8dd8d1ded06ce31ddee59e3ca314119a354386fa
Andres Puello <andres@pop-os.localdomain>
2021-12-03 19:39:14 -0500

    Inicio de pagina

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SanjuanShop
+# SanjuanShop
---
+++ wordpress.sh
@@ -0,0 +1,7 @@
+echo "# SanjuanShop" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/afrolino02/SanjuanShop.git
+git push -u origin main
